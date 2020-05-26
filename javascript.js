5aad19f7c551d0ffbf92ebe66173c8ea5efabfd9
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-05-26 17:22:49 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -4,9 +4,9 @@ console.log(`%cHey you sneaky little monkey, trying to take a look at `+
 `background: yellow; color: red; font-size: 35px; font-family: Arial; font-weight: bold;`);
 
 
-
-
-
+alert('THIS WEBSITE IS STILL UNDER DEV. DO NOT EXPECT TO BE PERFECT RIGHT NOW!!!!!!!');
+alert('DO YOU UNDERSTAND!?!?!');
+alert('OK GOOD YOU CAN SEE IT NOW.');
 
 
 
