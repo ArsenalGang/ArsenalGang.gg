775e739c708414ab83da494a4f276852977296f9
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-05-26 10:25:14 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-
+//I'm watching,...
+console.log(`%cHey you sneaky little monkey, trying to take a look at `+
+`the code in this website? Hahaha I'm always watching... ( ͡ʘ ͜ʖ ͡ʘ)`,
+`background: yellow; color: red; font-size: 35px; font-family: Arial; font-weight: bold;`);
 
 
 
