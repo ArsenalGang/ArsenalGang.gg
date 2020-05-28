09fc1210c96cfdd040932d85b2c256a87d9042a2
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-05-28 17:04:54 -0400

    Website ready after this commit
    Read the title

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
+// Detect mobile user and say sorry
+function isMobile() {
+return /(phone Ipad|pod I iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec IwOSBrowser|BrowserNG IWebos ISymbian|Windows Phone)/i.test(navigator.userAgent)
+}if(isMobile()){
+alert(`Hello! Unfortunely, Aresenal Gang's website has not yet been developed on mobile yet as it is currently a desktop only site,
+so the website is going to look really strange on your device! Sorry for any inconvenience :(`);
+};
+
+
 //I'm watching,...
 console.log(`%cHey you sneaky little monkey, trying to take a look at `+
 `the code in this website? Hahaha I'm always watching... ( ͡ʘ ͜ʖ ͡ʘ)`,
 `background: yellow; color: red; font-size: 35px; font-family: Arial; font-weight: bold;`);
 
 
-alert('WARNING: THIS WEBSITE IS STILL UNDER DEV. DO NOT EXPECT TO BE PERFECT RIGHT NOW!!!!!!!');
-alert('DO YOU UNDERSTAND!?!?!');
-alert('OK GOOD YOU CAN SEE IT NOW.');
 
 
 
